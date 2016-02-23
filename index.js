9e6d93d364e8a0faa0ea8c7885e4e56491504593
Valerian Saliou <valerian@valeriansaliou.name>
2016-02-23 15:00:37 +0100

    v1.1.0
    Signed-off-by: Valerian Saliou <valerian@valeriansaliou.name>

--- index.js
+++ index.js
@@ -19,6 +19,54 @@ var remove_logging  = require("./lib/removelogging");
 module.exports = function(opts) {
   opts = opts || {};
 
+  // Use console as the default namespace
+  if (!("namespace" in opts)) {
+    opts.namespace = [
+      "console",
+      "window.console"
+    ];
+  }
+
+  // Default methods
+  if (!("methods" in opts) || (typeof opts.methods !== "object")) {
+    opts.methods = [
+      "log",
+      "info",
+      "warn",
+      "error",
+      "assert",
+      "count",
+      "clear",
+      "group",
+      "groupEnd",
+      "groupCollapsed",
+      "trace",
+      "debug",
+      "dir",
+      "dirxml",
+      "profile",
+      "profileEnd",
+      "time",
+      "timeEnd",
+      "timeStamp",
+      "table",
+      "exception"
+    ];
+  }
+
+  if (!("verbose" in opts)) {
+    opts.verbose = true;
+  }
+
+  var regex_console = new RegExp(
+    ("(" + opts.namespace.join("|") + ")" +
+        ".(?:" + opts.methods.join("|") +
+        ")\\s{0,}\\([^;]*\\)(?!\\s*[;,]?\\s*\\/\\*\\s*" +
+        "RemoveLogging:skip\\s*\\*\\/)\\s{0,};?"),
+    "gi"
+  );
+
+  // Proceed transform
   return through.obj(function(file, encoding, callback) {
     if (file.isNull()) {
       return callback(null, file);
@@ -33,7 +81,9 @@ module.exports = function(opts) {
     }
 
     if (file.isStream()) {
-      var streamer = remove_logging.proceed(file, opts);
+      var streamer = remove_logging.proceed(
+        file, opts, regex_console
+      );
 
       streamer.on(
         "error", this.emit.bind(this, "error")
